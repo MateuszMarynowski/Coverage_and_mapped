bbc8969a43f376fa0e40f2447f134379d096ff04
Mateusz Marynowski <47397083+MateuszMarynowski@users.noreply.github.com>
2019-12-17 15:03:35 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 task_name="collect_hs_metrics"
+task_name=$(echo "$task_name" | sed -r 's/_/-/g')
 task_version="latest"
 python3 meta.py 'https://gitlab.com/intelliseq/workflows/raw/master/src/main/wdl/tasks/$task_name/$task_version/$task_name.wdl'
