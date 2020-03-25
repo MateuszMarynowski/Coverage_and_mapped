186b2fdf35a7d65b03fda28ee8b0097f5b630d2a
Mateusz <mateusz.marynowski97@gmail.com>
2020-03-25 14:10:02 +0100

    test

--- after-start.sh
+++ after-start.sh
@@ -3,4 +3,4 @@ date +%s > starttime
 echo "##################################################$task_name_with_index##################################################"
 >&2 echo "##################################################$task_name_with_index##################################################"
 docker_size=$(du -ach / | tail -n 1 | awk 'NR==1{print $1}')
-echo $docker_size > docker_size
+echo "$docker_size" > docker_size
