c29bcb7f1ef983f5c462afe1d657507195cea9f1
Mateusz Marynowski <47397083+MateuszMarynowski@users.noreply.github.com>
2020-03-30 13:26:23 +0200

    Update after-start.sh

--- after-start.sh
+++ after-start.sh
@@ -1,8 +1,8 @@
 ### start time
->&2 echo -e "###### stderr for after start script ######\n"
+>&2 echo -e "################################################## stderr for after start script ##################################################\n"
 docker_size=$(du -achx / | tail -n 1 | awk 'NR==1{print $1}')
 echo "$docker_size" > docker_size
 date +%s > starttime
 echo "##################################################$task_name_with_index##################################################"
->&2 echo -e "###### stderr for task $task_name_with_index ######\n"
+>&2 echo -e "################################################## stderr for task $task_name_with_index ##################################################\n"
 
